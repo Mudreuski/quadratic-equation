10ac4ae28b519d7dec688825c9e43d6bc7243aa8
Mudreuski <bahus1994@mail.ru>
2018-09-15 01:05:13 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	
+	let a = +equation.split(' * x^2')[0];
+	let workB = equation.split(' * x')[1];
+	let b = +(workB.substring(3,4)+workB.substring(5));
+	let workC = equation.split(' * x')[2];
+	let c = +(workC.substring(1,2)+workC.substring(3));
+	
+	let discr = b * b - 4 * a * c;
+	
+	let x1 = Math.round((-b+Math.sqrt(discr))/(2*a));
+	let x2 = Math.round((-1*b-Math.sqrt(discr))/(2*a));
+	
+	if (x1 > x2){
+		var answer = [x2,x1];     					
+	}
+	else{
+		var answer = [x1,x2];		
+	}
+	
+	return answer;
 }
